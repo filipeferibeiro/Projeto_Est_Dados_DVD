8f867ba84753e78b9fa90e61749afd3d3e406028
Guilherme Pereira <guilherme27.gp@gmail.com>
2017-04-25 12:07:13 -0300

    Corrigido o bug da função de busca por nome, adicionada a função de deletar(por nome ou código), adicionada a função de listar, adicionada as funções de alugar e devolver o filme, iniciada a função de salvar em .txt.

--- program.py
+++ program.py
@@ -4,6 +4,11 @@ import os, sys, time
 Locadora = BST()
 LDE = "Locadora de Estrutura\n"
 
+
+Locadora.insert(1,'nome','gui', 'disponível', 522)
+Locadora.insert(2,'007','gui', 'disponível', 522)
+Locadora.insert(-1,'hulk','gui', 'disponível', 522)
+
 print(LDE)
 login = input("Login: ")
 os.system("cls")
@@ -16,7 +21,7 @@ while True:
     while cond1 == True:
         os.system("cls")
         print(LDE)
-        print("Escolha uma opção:\n1 - Buscar filme\n2 - Listar todos os filmes\n3 - Adicionar filme\n4 - Remover filme\n5 - Sair\n")
+        print("Escolha uma opção:\n1 - Buscar filme\n2 - Listar todos os filmes\n3 - Adicionar filme\n4 - Remover filme\n5 - Alugar Filme\n6 - Devolver Filme\n7 - Sair\n")
         try:
             option = int(input())
             cond1 = False
@@ -39,25 +44,27 @@ while True:
                 if type_search == 1:
                     data = int(input("Código: "))
                 elif type_search == 2:
-                    data = input("Nome: ")
+                    data = input("Nome: ").lower()
+                    print(data)
                 elif type_search == 3:
                     data = input("Categoria: ")
                 elif type_search == 4:
                     cond1 = True
                     break
                 if type_search == 1 or type_search == 2 or type_search == 3:
-                    if Locadora.search(type_search, data) is None:
+                    if Locadora.search(type_search, str(data)) is not None:
+                        a, b, c, d, e = Locadora.search(type_search, data)
+                        os.system("cls")
+                        print(LDE)
+                        print("Código do DVD:", a, "\nNome do filme:", b, "\nGênero:", c, "\nSituação:", d,
+                              "\nValor do aluguel: R$", e, "\n")
+                        input("Pressione enter para continuar")
+                        cond1 = True
+                    else:
                         os.system("cls")
                         print(LDE, "\nFilme não encontrado.")
                         time.sleep(3)
                         cond2 = True
-                    else:
-                        a, b, c, d, e = Locadora.search(type_search, data)
-                        os.system("cls")
-                        print(LDE)
-                        print("Código do DVD:", a, "\nNome do filme:", b, "\nGênero:", c, "\nSituação:", d, "\nValor do aluguel:", e, "\n")
-                        input("Pressione enter para continuar")
-                        cond1 = True
                 else:
                     os.system("cls")
                     print(LDE, "\nPor favor, escolha algo válido.")
@@ -69,30 +76,179 @@ while True:
                 time.sleep(3)
                 cond2 = True
 
-    if option == 2:
-        print("Opção ainda não implementada. Tente novamente em futuras atualizações.")
+    elif option == 2:
+        os.system("cls")
+        print(LDE, '\n')
+        print("Total de filmes:", Locadora.get_total_nodes())
+        print("--------------------------------------------------------------------------------------")
+        Locadora.inOrderTraversal()
+        input('Pressione enter para continuar')
 
-    if option == 3:
+    elif option == 3:
         os.system("cls")
         print(LDE)
         code = int(input("Código: "))
         os.system("cls")
         print(LDE)
-        name = input("Filme: ")
-        name = name.lower()
+        name = input("Filme: ").lower()
         os.system("cls")
         print(LDE)
-        gender = input("Gênero: ")
-        gender = gender.lower()
+        gender = input("Gênero: ").lower()
         os.system("cls")
         print(LDE)
-        value = int(input("Valor: "))
+        value = float(input("Valor: "))
         os.system("cls")
         Locadora.insert(code, name, gender, "disponível", value)
         cond1 = True
         os.system("cls")
         print(LDE)
+        file = open('locadora.txt', 'a')
+        file.write("Código do DVD: " + str(code))
+        file.write("\nNome do filme: " + str(name))
+        file.write("\nGênero: " + str(gender))
+        file.write("\nSituação: " + 'disponível')
+        file.write("\nValor do aluguel: R$" + str(value))
+        file.write("\n-------------------------------\n")
+        file.close()
         print("Filme adicionado.")
         time.sleep(2)
-    if option == 5:
+    elif option == 4:
+        os.system("cls")
+        print(LDE)
+        cond5 = True
+        while cond5 == True:
+            try:
+                os.system("cls")
+                print(LDE)
+                type_search = int(input("1 - Por código\n2 - Por nome\n3 - Voltar\n\n"))
+                cond5 = False
+                os.system("cls")
+                print(LDE)
+                if type_search == 1:
+                    data = int(input("Código: "))
+                elif type_search == 2:
+                    data = input("Nome: ").lower()
+                    print(data)
+                elif type_search == 3:
+                    cond1 = True
+                    break
+                if type_search == 1 or type_search == 2:
+                    if Locadora.search(type_search, str(data)) is not None:
+                        a, b, c, d, e = Locadora.search(type_search, data)
+                        os.system("cls")
+                        print(LDE)
+                        Locadora.remove_node(a)
+                        print("DVD removido com Sucesso.")
+                        time.sleep(3)
+                        cond1 = True
+                    else:
+                        os.system("cls")
+                        print(LDE, "\nFilme não encontrado.")
+                        time.sleep(3)
+                        cond5 = True
+                else:
+                    os.system("cls")
+                    print(LDE, "\nPor favor, escolha algo válido.")
+                    time.sleep(3)
+                    cond5 = True
+            except ValueError:
+                os.system("cls")
+                print(LDE, "\nPor favor, escolha algo válido.")
+                time.sleep(3)
+                cond5 = True
+
+    elif option == 5:
+        os.system("cls")
+        print(LDE)
+        cond3 = True
+        while cond3 == True:
+            try:
+                os.system("cls")
+                print(LDE)
+                type_search = int(input("1 - Por código\n2 - Por nome\n3 - Voltar\n\n"))
+                cond3 = False
+                os.system("cls")
+                print(LDE)
+                if type_search == 1:
+                    data = int(input("Código: "))
+                elif type_search == 2:
+                    data = input("Nome: ").lower()
+                elif type_search == 3:
+                    cond1 = True
+                    break
+                if type_search == 1 or type_search == 2:
+                    if Locadora.rent(type_search, str(data)) == True:
+                        os.system("cls")
+                        print(LDE)
+                        print('Filme Alugado!')
+                        time.sleep(3)
+                        cond1 = True
+                    elif Locadora.rent(type_search, str(data)) == False:
+                        print('Este filme já está alugado!')
+                        time.sleep(3)
+                        cond1 = True
+                    else:
+                        os.system("cls")
+                        print(LDE, "\nEste filme não está no catálogo.")
+                        time.sleep(3)
+                        cond3 = True
+                else:
+                    os.system("cls")
+                    print(LDE, "\nPor favor, escolha algo válido.")
+                    time.sleep(3)
+                    cond3 = True
+            except ValueError:
+                os.system("cls")
+                print(LDE, "\nPor favor, escolha algo válido.")
+                time.sleep(3)
+                cond3 = True
+                
+    elif option == 6:
+        os.system("cls")
+        print(LDE)
+        cond4 = True
+        while cond4 == True:
+            try:
+                os.system("cls")
+                print(LDE)
+                type_search = int(input("1 - Por código\n2 - Por nome\n3 - Voltar\n\n"))
+                cond4 = False
+                os.system("cls")
+                print(LDE)
+                if type_search == 1:
+                    data = int(input("Código: "))
+                elif type_search == 2:
+                    data = input("Nome: ").lower()
+                    print(data)
+                elif type_search == 3:
+                    cond1 = True
+                    break
+                if type_search == 1 or type_search == 2:
+                    if Locadora.give_back_movie(type_search, str(data)) == True:
+                        os.system("cls")
+                        print(LDE)
+                        print('Filme Devolvido!')
+                        time.sleep(3)
+                        cond1 = True
+                    elif Locadora.give_back_movie(type_search, str(data)) == False:
+                        print('Este filme já está disponível!')
+                        time.sleep(3)
+                        cond1 = True
+                    else:
+                        os.system("cls")
+                        print(LDE, "\nEste filme não está no catálogo.")
+                        time.sleep(3)
+                        cond4 = True
+                else:
+                    os.system("cls")
+                    print(LDE, "\nPor favor, escolha algo válido.")
+                    time.sleep(3)
+                    cond4 = True
+            except ValueError:
+                os.system("cls")
+                print(LDE, "\nPor favor, escolha algo válido.")
+                time.sleep(3)
+                cond4 = True
+        
+    elif option == 7:
         break
